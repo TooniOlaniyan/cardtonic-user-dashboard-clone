be90cf31a78155ab93c946308888ff9dd39ddf4a
TooniOlaniyan <asalamlatif@gmail.com>
2022-09-03 19:57:44 +0100

    clean up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import Loader from './component/Loader';
 
-import { Counter } from './features/counter/Counter';
 
 
 function App() {
---
+++ src/features/counter/userAuthSlice.js
@@ -0,0 +1,9 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+
+const initialState = {
+  value: 0,
+  status: 'idle',
+};
+
+
