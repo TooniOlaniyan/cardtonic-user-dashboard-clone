3127b8137a64ec3433a249f1c595077512be54d5
TooniOlaniyan <asalamlatif@gmail.com>
2022-09-04 22:50:15 +0100

    signin page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,8 @@
 import React , {useState} from 'react'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 import RegisterNav from '../component/RegisterNav'
+import { BsArrowRightShort } from 'react-icons/bs'
 import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai'
 
 function SignIn() {
@@ -16,6 +18,9 @@ function SignIn() {
           [e.target.id]: e.target.value,
         }))
       }
+       const handleSubmit = (e) => {
+         e.prevetDefault()
+       }
   return (
     <div>
       <RegisterNav />
@@ -56,7 +61,7 @@ function SignIn() {
                 value={password}
                 id='password'
                 type={isRevealed ? 'text' : 'password'}
-                placeholder='Create a Password'
+                placeholder='Password'
               />
             </div>
             <div className='submit'>
@@ -65,10 +70,9 @@ function SignIn() {
               </button>
 
               <p>
-                Got an account?{' '}
-                <Link to='/sign-in' style={{ textDecoration: 'none' }}>
-                  <span>Sign In</span>
-                </Link>
+                
+                Forgot your password?
+              
               </p>
             </div>
           </form>
@@ -77,6 +81,106 @@ function SignIn() {
     </div>
   )
 }
+const MainContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .middleContent {
+    padding: 1rem 5rem;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    gap: 4rem;
+    align-items: flex-start;
+    width: 100vw;
+
+    .text {
+      width: fit-content;
+
+      p {
+        font-size: 55px;
+        font-weight: 900;
+        color: ${({ theme }) => theme.colors.darkblue};
+      }
+      .nothing {
+        font-size: 20px;
+        font-weight: 900;
+        color: ${({ theme }) => theme.colors.gray};
+      }
+    }
+  }
+`
+
+const Form = styled.div`
+  display: flex;
+  width: 45rem;
+  background-color: white;
+  border-radius: 2rem;
+  form {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    margin: 0 auto;
+    padding: 4rem;
+    .formInputs {
+      width: 100%;
+      position: relative;
+      .passwordEye {
+        position: absolute;
+        top: 1.5rem;
+        right: 1.5rem;
+        font-size: 23px;
+        cursor: pointer;
+        color: ${({ theme }) => theme.colors.textcolor};
+      }
+
+      input {
+        border: 1px solid ${({ theme }) => theme.colors.lightblue};
+        border-radius: 1rem;
+        margin-bottom: 1.5rem;
+        padding: 1.3rem 2rem;
+        font-size: 18px;
+        width: 100%;
+        &:focus {
+          outline: none;
+          background-color: transparent;
+        }
+        &::placeholder {
+          color: ${({ theme }) => theme.colors.inputColor};
+        }
+      }
+    }
+    .submit {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin-top: 1rem;
+
+      width: 100%;
+      button {
+        display: flex;
+        justify-content: center;
+        gap: 1rem;
+        align-items: center;
+        padding: 1rem;
+        border-radius: 1.3rem;
+        font-size: 20px;
+        color: ${({ theme }) => theme.colors.lightblue1};
+        background-color: ${({ theme }) => theme.colors.black};
+        cursor: pointer;
+        .icon {
+          font-size: 30px;
+        }
+      }
+      p {
+        font-size: 16px;
+        color: ${({ theme }) => theme.colors.black};
+      }
+    }
+  }
+`
 
 
 export default SignIn
