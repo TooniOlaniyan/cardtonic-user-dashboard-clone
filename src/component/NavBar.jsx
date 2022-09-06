d01b139486993b83ba43467c5aef229cf19463d8
TooniOlaniyan <asalamlatif@gmail.com>
2022-09-06 21:36:27 +0100

    logout popup

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import logOut from '../asset/images/logOut.svg'
 import notification from '../asset/images/notification.svg'
 import {useNavigate, UseNavigate} from 'react-router-dom'
  
 function NavBar() {
+  const [isOpen , setIsOpen] = useState(false)
   const navigate = useNavigate()
   const handleClick = () => {
-    navigate('/')
+    setIsOpen(true)
 
   }
   return (
@@ -23,6 +24,17 @@ function NavBar() {
           <img src={notification} alt='' />
         </div>
       </div>
+      {isOpen && (
+        <div className='logOutModal'>
+          <div>
+            <p>Are you sure you want to log out?</p>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <img src="" alt="" />
+          </div>
+        </div>
+      )}
     </Navigation>
   )
 }
@@ -33,6 +45,18 @@ const Navigation = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  .logOutModal {
+    height: 100vh;
+    width: 100vw;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background: rgba(0, 0, 0, 0.7);
+    backdrop-filter: blur(5px);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
   .greetings {
     align-self: flex-end;
     p {
