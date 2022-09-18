70bbc008ad53154014d1f992a28c42aab12309a3
TooniOlaniyan <asalamlatif@gmail.com>
2022-09-18 14:17:52 +0100

    size change

--- src/component/Switch.jsx
+++ src/component/Switch.jsx
@@ -4,7 +4,7 @@ import DarkModeToggle from "react-dark-mode-toggle";
 function Switch() {
 const [isDarkMode, setIsDarkMode] = useState(() => false)
   return (
-    <DarkModeToggle onChange={setIsDarkMode} checked={isDarkMode} size={80} />
+    <DarkModeToggle onChange={setIsDarkMode} checked={isDarkMode} size={70} />
   )
 }
 
