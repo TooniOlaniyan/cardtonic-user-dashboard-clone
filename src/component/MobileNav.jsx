be230fe3e442feb5b6d8b63c69433d5bd065100d
TooniOlaniyan <asalamlatif@gmail.com>
2022-09-21 16:49:52 +0100

    hamburger animate presense

--- src/component/MobileNav.jsx
+++ src/component/MobileNav.jsx
@@ -49,30 +49,32 @@ function MobileNav({isOpened}) {
           className='bgContainer'
         ></motion.div>
       )}
-      {isOpened && (
-        <motion.div
-          variants={menuVarient}
-          initial='initial'
-          animate='final'
-          exit={{ x: [0, -130, 1000] }}
-          className='links'
-        >
-          <Link to='/dashboard'>Dashboard</Link>
-          <Link to='/start-trade'>Start Trade</Link>
-          <Link to='/transactions'>Transaction</Link>
-          <Link to='/wallet'>Wallet</Link>
-          <Link to='/calculator'>Calculator</Link>
-          <Link to='/settings'>Settings</Link>
-          <Link to='/dashboard'>Support</Link>
-          <div className='clicks'>
-            <div className='icon'>
-              <img src={star} alt='' />
-              <img src={reward} alt='' />
+      <AnimatePresence>
+        {isOpened && (
+          <motion.div
+            variants={menuVarient}
+            initial='initial'
+            animate='final'
+            exit={{ x: [0, -130, 1000] }}
+            className='links'
+          >
+            <Link to='/dashboard'>Dashboard</Link>
+            <Link to='/start-trade'>Start Trade</Link>
+            <Link to='/transactions'>Transaction</Link>
+            <Link to='/wallet'>Wallet</Link>
+            <Link to='/calculator'>Calculator</Link>
+            <Link to='/settings'>Settings</Link>
+            <Link to='/dashboard'>Support</Link>
+            <div className='clicks'>
+              <div className='icon'>
+                <img src={star} alt='' />
+                <img src={reward} alt='' />
+              </div>
+              <img className='icons' src={logOut} alt='' />
             </div>
-            <img className='icons' src={logOut} alt='' />
-          </div>
-        </motion.div>
-      )}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </Container>
   )
 }
@@ -82,7 +84,7 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  height: 100vh;
+  /* height: 100vh; */
   padding: 0 5.5rem;
   width: 100vw;
   z-index: 1;
@@ -91,7 +93,6 @@ const Container = styled.div`
   left: 0;
   gap: 2rem;
   transition: all 0.6s cubic-bezier(0.68, -0.55, 0.265, 1.55);
-  /* background-color: red; */
   a {
     text-decoration: none;
     color: white;
@@ -103,7 +104,7 @@ const Container = styled.div`
     width: 30px;
     position: fixed;
     top: 45px;
-    right: 15px;
+    right: 25px;
     background: #000;
     border-radius: 50%;
     z-index: 0;
@@ -116,6 +117,7 @@ const Container = styled.div`
     flex-direction: column;
     gap: 3rem;
     z-index: 3;
+    margin-top: 50%;
     transition: all 0.6s cubic-bezier(0.68, -0.55, 0.265, 1.55);
 
     width: 100%;
